4ce22b2b7324feb9c02e262c6a06a36207688a66
Ali Asoodeh <aliasodefard@gmail.com>
2022-05-15 21:53:06 +0430

    Update crt.sh
    update crt.sh

--- crt.sh
+++ crt.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-domain=$1
+TARGET="$1"
 
-if [ $# -lt 1 ]
-then
-    echo "Usage: ./crt.sh example.com)"
-    exit 0
-fi
- 
-curl -s "https://crt.sh/?q=%.${domain}&output=json" | jq -r '.[] | .comman_name, .name_value ' | sed 's/\*\.//g' | sort -u | uniq > ${domain}_subdomains.txt &&
-echo "Result ${domain}_subdomain.txt"
-
+curl -s https://crt.sh/\?q\=%.$TARGET\&output\=json > /tmp/curl.out
+cat /tmp/curl.out | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > $TARGET-crt.txt
+cat $TARGET-crt.txt
